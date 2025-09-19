c154df250d4f9fd67919192808dd91e2b3d5be85
Pavliuk <yurapavliuk06@gmail.com>
2025-09-19 11:06:50 +0300

    3 commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,13 +4,26 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.SecurityContext;
+import java.security.Principal;
 @Path("/hello")
 public class GreetingResource {
-
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String whoAmI(@Context SecurityContext securityContext) {
+        Principal userPrincipal = securityContext.getUserPrincipal();
+        if (userPrincipal != null) {
+            return userPrincipal.getName();
+        } else {
+            return "anonymous";
+        }
+    }
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
     }
 }
+
